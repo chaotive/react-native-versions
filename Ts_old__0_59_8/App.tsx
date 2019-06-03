64446469a268219c91fb6fa5ab575bd01212e298
chaotive <leonardomontenegro123@gmail.com>
2019-06-03 17:32:13 -0400

    Init basic Typescript for 0.59.8 project

--- Ts_old__0_59_8/App.tsx
+++ Ts_old__0_59_8/App.tsx
@@ -6,12 +6,14 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react'
+import { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
+    'Please don\'t update this project dependencies, we use to track changes on updating react-native.\n\n' +
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
@@ -21,8 +23,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Welcome to React Native 0.59.8!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx.</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
